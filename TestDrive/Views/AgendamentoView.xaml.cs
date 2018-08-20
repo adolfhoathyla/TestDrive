46fe525911b8f27899b23b028c70234502687615
Athyla <adolfo.athyla@gmail.com>
2018-08-20 17:39:46 -0300

    Metade aula 3

---
+++ TestDrive/Views/AgendamentoView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TestDrive.Views
+{
+    public partial class AgendamentoView : ContentPage
+    {
+        public AgendamentoView(Veiculo veiculo)
+        {
+            InitializeComponent();
+            this.Title = veiculo.Nome;
+        }
+    }
+}
---
+++ TestDrive/Views/DetalheView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TestDrive.Views
+{
+    public partial class DetalheView : ContentPage
+    {
+        public Veiculo veiculo
+        {
+            get;
+            set;
+        }
+
+        public DetalheView(Veiculo veiculo)
+        {
+            InitializeComponent();
+
+            this.Title = veiculo.Nome;
+            this.veiculo = veiculo;
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new AgendamentoView(this.veiculo));
+        }
+    }
+}
